8908d3d8c1aa2e77629c7e2e3e0e438f02aa264f
NsimDaghash <89819123+NsimDaghash@users.noreply.github.com>
2022-10-26 23:49:12 +0300

    build square
    I will give you an integer. Give me back a shape that is as long and wide as the integer. The integer will be a whole number between 1 and 50.

---
+++ BuilSquare.js
@@ -0,0 +1,23 @@
+/*
+Build a square
+I will give you an integer. Give me back a shape that is as long and wide as the integer. The integer will be a whole number between 1 and 50.
+
+Example
+n = 3, so I expect a 3x3 square back just like below as a string:
+
++++
++++
++++
+*/
+function generateShape(integer){
+ let str="";
+  for(let i=0;i<integer;i++){
+    for(let j=0;j<integer;j++){
+      str=str+"+";
+      }  
+    if(i == integer-1){
+      return str;
+    }
+    str=str+"\n";
+  }
+}
